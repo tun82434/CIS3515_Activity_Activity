6bce2d5e5aeab9f5a86fa1cd3f0a6ee6e0d23d45
tun82434 <tun82434@temple.edu>
2025-02-18 12:20:02 -0500

    completed

--- app/src/main/java/edu/temple/activities/MainActivity.kt
+++ app/src/main/java/edu/temple/activities/MainActivity.kt
@@ -1,12 +1,16 @@
 package edu.temple.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.Display
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+
+const val SIZE_KEY = "someKeyValue"
 
 class MainActivity : AppCompatActivity() {
 
@@ -24,7 +28,19 @@ class MainActivity : AppCompatActivity() {
 
             // TODO Step 2: Implement lambda body to launch new activity and pass value
             adapter = TextSizeAdapter(textSizes){
-
+                /*
+                val launchIntent = Intent(this@MainActivity, DisplayActivity::class.java)
+                launchIntent.putExtra(SIZE_KEY, it)
+                startActivity(launchIntent)
+                */
+                startActivity(
+                    Intent(
+                        this@MainActivity,
+                        DisplayActivity::class.java
+                    ).apply {
+                        putExtra(SIZE_KEY,it.toFloat())
+                    }
+                )
             }
             layoutManager = LinearLayoutManager(this@MainActivity)
         }
@@ -36,12 +52,14 @@ class MainActivity : AppCompatActivity() {
 
 
 /* Convert to RecyclerView.Adapter */
-class TextSizeAdapter (private val textSizes: Array<Int>, callback: (Int)->Unit) : RecyclerView.Adapter<TextSizeAdapter.TextSizeViewHolder>() {
+class TextSizeAdapter (private val textSizes: Array<Int>, private val callback: (Int)->Unit) : RecyclerView.Adapter<TextSizeAdapter.TextSizeViewHolder>() {
 
     // TODO Step 1: Complete onClickListener to return selected number
     inner class TextSizeViewHolder(val textView: TextView) : RecyclerView.ViewHolder (textView) {
         init {
-            textView.setOnClickListener {  }
+            textView.setOnClickListener {
+                callback(textSizes[adapterPosition])
+            }
         }
     }
 
